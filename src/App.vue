8c373841401e5edf38bd650624708139d24131f9
jun <junpeeei0407@gmail.com>
2022-12-25 20:45:57 +0900

    tutorial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+const placeHolder = 'ここに入力'
+const onInput = (e) => {
+  text.value = e.target.value;
+}
+</script>
+
+<template>
+  <input :value='text' @input='onInput' :placeholder="placeHolder">
+  <p>{{ text }}</p>
+</template>
