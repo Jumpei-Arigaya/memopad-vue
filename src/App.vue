ec8bd00be7db97399c2e65003b99313f42727c2f
jun <junpeeei0407@gmail.com>
2022-12-26 01:08:04 +0900

    vue基礎 配列処理

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,34 @@ import { ref } from 'vue'
 
 const text = ref('')
 const placeHolder = 'ここに入力'
-const onInput = (e) => {
-  text.value = e.target.value;
+const awesome = ref(false)
+const onClickToggle = () => {
+  awesome.value = !awesome.value
 }
+const newTodo = ref('');
+let id = 0;
+const todos = ref([
+  { id: id++, text: 'HTMLの勉強' },
+  { id: id++, text: 'Typescriptのコーディング' },
+  { id: id++, text: 'キャンプ用品の準備' },
+])
+
+const addTodo = () => {
+  todos.value.push({ id: 1, text: '新規追加分' })
+}
+
+console.log(todos)
+
 </script>
 
 <template>
-  <input :value='text' @input='onInput' :placeholder="placeHolder">
+  <input v-model="text" :placeholder="placeHolder">
   <p>{{ text }}</p>
+  <button @click="onClickToggle">切り替えボタン</button>
+  <h1 v-if="awesome">切り替え表示領域</h1>
+  <h1 v-else>oh...(^o^)</h1>
+
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.text }}</li>
+  </ul>
 </template>
